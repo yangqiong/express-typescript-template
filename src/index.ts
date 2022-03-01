64b057827e34e314f99ba1cf4e6ae986cc01d1de
yangqiong <yangqionggo@163.com>
2022-03-01 17:25:03 +0800

    feat: dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.listen()
+app.use("/",async (req, res) => {
+    res.status(200).end("Hello World")
+})
+
+app.listen(process.env.PORT)
---
+++ src/types/environment.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    MONGO_URI: string;
+  }
+}
